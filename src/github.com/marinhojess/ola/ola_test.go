dcf1d2a3bbe4cf72a8559bf7ad58c4bfb5625f6b
jessica.costa <jessica.costa@sensedia.com>
2020-11-24 14:03:01 -0300

    Refatoraçao 2

--- src/github.com/marinhojess/ola/ola_test.go
+++ src/github.com/marinhojess/ola/ola_test.go
@@ -3,23 +3,23 @@ package main
 import "testing"
 
 func TestOla(t *testing.T) {
-	t.Run("diz olá para as pessoas", func(t *testing.T) {
+	verificaMensagemCorreta := func(t *testing.T, resultado, esperado string){
+		t.Helper()
+		if resultado != esperado{
+			t.Errorf("resultado '%s', esperado '%s'", resultado, esperado)
+		}
+	}
+
+	t.Run("diz olá para as pessoas", func(t *testing T){
 		resultado := Ola("Chris")
 		esperado := "Olá, Chris"
-
-		if resultado != esperado {
-			t.Errorf("resultado '%s', esperado '%s'", resultado, esperado)
-		}
-
+		verificaMensagemCorreta(t, resultado, esperado)
 	})
 
-	t.Run("diz 'Olá, mundo' quando uma string vazia por passada", func(t *testing.T) {
+	t.Run("'Mundo' como padrão para 'string' vazia", func(t *testing.T){
 		resultado := Ola("")
-		esperado := "Olá, mundo"
-
-		if resultado != esperado {
-			t.Errorf("resultado '%s', esperado '%s'", resultado, esperado)
-		}
-
+		esperado := "Olá, Mundo"
+		verificaMensagemCorreta(t, resultado, esperado)
 	})
+
 }
