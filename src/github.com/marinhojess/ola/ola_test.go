91670a153821cebf6c5269596e72d5b7c1544907
jessica.costa <jessica.costa@sensedia.com>
2020-11-24 10:45:26 -0300

    My first commit

---
+++ src/github.com/marinhojess/ola/ola_test.go
@@ -0,0 +1,13 @@
+package main
+
+import "testing"
+
+func TestOla(t *testing.T) {
+	resultado := Ola("Chris")
+	esperado := "Olá, Chris"
+
+	if resultado != esperado {
+		t.Errorf("resultado '%s', esperado '%s'", resultado, esperado)
+	}
+
+}
